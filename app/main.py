639e41757a297cf3520c39115eb7562b6a745121
alx_27 <didieralejandromejia83@gmail.com>
2023-12-11 15:20:57 -0500

    Create the new table category in the database dbpythonapi

--- app/main.py
+++ app/main.py
@@ -8,6 +8,21 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost:3306/bdpythonapi'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Category(db.Model):
+    cat_id = db.Column(db.Integer, primary_key=True)
+    cat_nom = db.Column(db.String(100))
+    cat_desp = db.Column(db.String(100))
+    
+    def __init__(self, cat_nom, cat_desp):
+        self.cat_nom = cat_nom
+        self.cat_desp = cat_desp
+        
+with app.app_context():
+    db.create_all()
+
 #welcome message
 @app.route("/", methods=["GET"])
 def index():
